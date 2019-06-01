aa107b1e895b54e5b5463a083f504423af0dfd4c
taraschuiko <taraschuiko@gmail.com>
2019-06-01 12:47:29 +0300

    Changed title to typography, added styles, lorem ipsum paragraph and grid item sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 import "./App.css";
-import { Container, Button, Grid, Paper } from "@material-ui/core";
+import { Container, Button, Grid, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  paper: {
+    padding: "8px"
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <Container>
-      <h1>Material UI</h1>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <Typography variant="h3" component="h1">
+        Material UI
+      </Typography>
       <Grid container spacing={3}>
-        <Grid item xs="6">
-          <Paper>Hi</Paper>
+        <Grid item xs={12} sm={8}>
+          <Paper className={classes.paper}>
+            <Typography paragraph>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus sit facilis beatae pariatur, unde tempora neque ullam
+              saepe, corporis at odit distinctio cum impedit ut, quaerat quo
+              soluta nihil! Et.
+            </Typography>
+            <Button variant="contained" color="primary">
+              Hello World
+            </Button>
+          </Paper>
         </Grid>
         <Grid item xs>
-          <Paper>Hello</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper>What's good</Paper>
+          <Paper className={classes.paper}>What's good</Paper>
         </Grid>
       </Grid>
     </Container>
