03b9f1a941e8326868872285dfb0df5c06258ef5
taraschuiko <taraschuiko@gmail.com>
2019-06-01 11:10:30 +0300

    Created simple app with Container, title and Button

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { Container, Button } from "@material-ui/core";
+
+function App() {
+  return (
+    <Container>
+      <h1>Material UI</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
